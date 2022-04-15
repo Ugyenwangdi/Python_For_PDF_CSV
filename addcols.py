6c3c49aacce1a68143f1dbdcfc8f72e6aad59a46
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2022-04-15 16:17:52 +0600

    update for csv table update

---
+++ addcols.py
@@ -0,0 +1,96 @@
+
+
+################################ Extract the rows of csv file and store it in the list so that I can update the data in the rows like extracting the dates and regions later ##############
+# Python program to read CSV file line by line
+# import necessary packages
+import csv
+
+def update_csv(name):
+
+    ls = []
+    # Open file
+    with open('CSVremovedRepeated' + '\\' + '{}.csv'.format(name),'r') as file_obj:
+        
+        # Create reader object by passing the file
+        # object to reader method
+        reader_obj = csv.reader(file_obj)
+        
+        # Iterate over each row in the csv
+        # file using reader object
+        for row in reader_obj:
+            ls.append(row)
+    # print(ls)
+
+
+    # Extract the new data from heading
+    new_dta = []
+    for i in ls:
+        header = i[0]
+        header = header.split("#")[0]
+        header = header.split("NEW PRICI NG DETA ILS")
+        header = header[1].split(":")
+        header = header[1:]
+        # print(header)
+        region = header[0].replace("Product", "")
+        # print(region)
+        product = header[1].replace("Approved Date", "")
+        # print(product)
+        approved_date = header[2].replace("\n", "")
+        # print(approved_date)
+        
+        new_dta = [region, product, approved_date]
+        # print(new_dta)
+        break
+
+    ############### From the above list to delete the first item for each list item ######
+    for i in ls:
+        i = i.pop(0)
+    # print(ls)
+
+
+    ######### prepare a column and rows #############
+
+    # extract column 1 for the column header
+    cols = ls[1]
+
+    newlst = ['Region', 'Product', 'Approved_Date']
+    newcols = newlst + cols  # ['Region', 'Product', 'Approved_Date', 'Station', 'RSP/KL', 'RSP/L']
+    # print(newcols)
+
+    # select rows for row values
+    # insert values for 'Region', 'Product', 'Approved_Date' in the row_data list
+    row_data = ls[2:]
+    new_rows = []
+    for item in row_data:
+        item = new_dta + item
+        new_rows.append(item)
+
+        # print(item)
+    # print(new_rows[:5])
+
+
+
+
+    # ############ ############### Create a csv file with new rows and columns added ##### ########
+    # import csv
+        
+    # # writing to csv file
+
+    with open('AddedColAndRowsCSV' + '\\' + '{}.csv'.format(name), 'w', newline='') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+            
+        # writing the fields
+        csvwriter.writerow(newcols)
+            
+        # writing the data rows
+        csvwriter.writerows(new_rows)
+
+# do it for all 20 files
+for i in range(20):
+    i += 1
+    try:
+        update_csv(i)
+    finally:
+        continue
+
--- pdftoxlsx.py
+++ pdftoxlsx.py
@@ -5,93 +5,46 @@
 
 
 import tabula as tb
-import pandas as pd
-import os
 
-# # To convert pdf files to csv files with file names = 1, 2, 3,...., n
-# for x in range(20):
-#     x += 1
-#     try:
-#         file = "FuelData"+"\\"+"{}.pdf".format(x)
-#         table = tb.read_pdf(file, pages='all')
+def convert_to_csv():
+    # To convert pdf files to csv files with file names = 1, 2, 3,...., n
+    for x in range(20):
+        x += 1
+        try:
+            file = "FuelData"+"\\"+"{}.pdf".format(x)
+            table = tb.read_pdf(file, pages='all')
 
-#         # csv file
-#         csv_table = tb.convert_into(file, 'CSVconverted'+'\\'+'{}.csv'.format(x), pages='all')
-#     finally:
-#         continue
+            # csv file
+            csv_table = tb.convert_into(file, 'CSVconverted'+'\\'+'{}.csv'.format(x), pages='all')
+        finally:
+            continue
 
+def remove_repeated():
 
-# # To remove the duplicates from from the above converted files
-# for i in range(11):
-#     if i != 0:
-#         with open('CSVconverted' + '\\' + '{}.csv'.format(i),'r') as in_file, open('CSVremovedRepeated' + '\\' + '{}.csv'.format(i),'w') as out_file:
-#             seen = set() # set 
-#             for line in in_file:
-#                 if line in seen: 
-#                     continue # skip duplicate
+    # To remove the duplicates from from the above converted files
+    for i in range(20):
+        i += 1
+        try:
+            with open('CSVconverted' + '\\' + '{}.csv'.format(i),'r') as in_file, open('CSVremovedRepeated' + '\\' + '{}.csv'.format(i),'w') as out_file:
+                seen = set() # set 
+                for line in in_file:
+                    if line in seen: 
+                        continue # skip duplicate
 
-#                 seen.add(line)
-#                 out_file.write(line)
-#                 # out_file.close()
-
-
-
-
-# ################################ List the rows to add to csv file ##############
-# # Python program to read CSV file line by line
-# # import necessary packages
-# import csv
-
-# ls = []
-# # Open file
-# with open('CSVremovedRepeated' + '\\' + '{}.csv'.format(1),'r') as file_obj:
-	
-# 	# Create reader object by passing the file
-# 	# object to reader method
-# 	reader_obj = csv.reader(file_obj)
-	
-# 	# Iterate over each row in the csv
-# 	# file using reader object
-# 	for row in reader_obj:
-# 		ls.append(row)
-# print(ls)
-# print(item[0].split("#") for item in ls)
+                    seen.add(line)
+                    out_file.write(line)
+                    # out_file.close()
+        finally:
+            continue
 
 
+if __name__ == "__main__":
 
-#################################### Add to Final file #################
-# Write CSV file final
-
-# Python program to demonstrate
-# writing to CSV
-
-
-# import csv
-	
-# # field names
-# fields = ['Station', 'Region', 'Product', 'Approved Date', 'RSP/KL', 'RSP/L'] # , 'RSP/KL', 'RSP/L'
-	
-# # data rows of csv file
-# rows = [ ['Nikhil', 'COE', '2', '9.0', '2', '9.0'],
-# 		['Sanchit', 'COE', '2', '9.1', '2', '9.0'],
-# 		['Aditya', 'IT', '2', '9.3', '2', '9.0'],
-# 		['Sagar', 'SE', '1', '9.5', '2', '9.0'],
-# 		['Prateek', 'MCE', '3', '7.8', '2', '9.0'],
-# 		['Sahil', 'EP', '2', '9.1', '2', '9.0']]
-	
-# # name of csv file
-# filename = "bhutan_fuel_prices.csv"
-	
-# # writing to csv file
-# with open(filename, 'w', newline='') as csvfile:
-# 	# creating a csv writer object
-# 	csvwriter = csv.writer(csvfile)
-		
-# 	# writing the fields
-# 	csvwriter.writerow(fields)
-		
-# 	# writing the data rows
-# 	csvwriter.writerows(rows)
+    try:
+        convert_to_csv()
+        remove_repeated()
+    except:
+        print("Couldn't perform convert")
 
 
 
@@ -102,7 +55,8 @@ import os
 
 
 
-
+# import pandas as pd
+# import os
 
 # from pathlib import Path
 
