57d023eca6b3957a3cb2de274ca4db64cc297dd5
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2022-04-17 11:32:02 +0600

    final

--- pdftoxlsx.py
+++ pdftoxlsx.py
@@ -8,7 +8,7 @@ import tabula as tb
 
 def convert_to_csv():
     # To convert pdf files to csv files with file names = 1, 2, 3,...., n
-    for x in range(20):
+    for x in range(1203):
         x += 1
         try:
             file = "FuelData"+"\\"+"{}.pdf".format(x)
@@ -22,7 +22,7 @@ def convert_to_csv():
 def remove_repeated():
 
     # To remove the duplicates from from the above converted files
-    for i in range(20):
+    for i in range(1203):
         i += 1
         try:
             with open('CSVconverted' + '\\' + '{}.csv'.format(i),'r') as in_file, open('CSVremovedRepeated' + '\\' + '{}.csv'.format(i),'w') as out_file:
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         convert_to_csv()
         remove_repeated()
     except:
-        print("Couldn't perform convert")
+        pass
 
 
 
